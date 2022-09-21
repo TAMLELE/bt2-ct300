90616dc077281f37f68a52401ed4a00e76e0be76
TAMLELE <104675352+TAMLELE@users.noreply.github.com>
2022-09-21 17:12:18 +0700

    see you a again

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void main(){
     char str1[16] = "Hello ";
-    char str2[16] = "World... ";
+    char str2[16] = "World ";
     printf(" %s\n%s", str1, str2);
     return 0;
 }
